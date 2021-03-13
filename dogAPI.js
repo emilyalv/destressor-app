36c18590cb2b48a5170143a2976a2fe1ca1a04c9
johnhumms <johnehummeljr@gmail.com>
2021-03-13 13:45:28 -0600

    Added HTML, CSS, and JS files for the dog api

---
+++ dogAPI.js
@@ -0,0 +1,36 @@
+//fetches breed list
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await response.json();
+    createBreedList(data.message);
+}
+
+start()
+
+//creates select element
+function createBreedList(breedlist) { 
+    document.getElementById("breed").innerHTML = ` 
+    <select onchange="loadByBreed(this.value)">
+            <option>Choose a dog breed</option>
+            ${Object.keys(breedlist).map(function (breed) {
+                return `<option>${breed}</option>`
+            }).join('')}
+        </select>
+    `
+}
+
+//fetches data according to breed selected by user
+async function loadByBreed(breed) {
+    if (breed != "Choose a dog breed") {
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        const data = await response.json()
+        createDogPic(data.message)
+        //got message from console
+    }
+}
+
+function createDogPic(images) {
+    document.getElementById("dogPic").innerHTML=`
+    <div class="dogPicImg" style="background-image: url('${images[0]}')"></div>
+    `
+}
