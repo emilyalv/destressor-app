d3c069877f018b033219306fe17381d3dfe72b8e
emilyalv <emilyaalv@gmail.com>
2021-03-15 20:27:10 -0500

    youtube API with search bar

---
+++ assets/js/youtubeAPI.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    var search = $('#search').val; 
+
+    $("form").submit(function(event){
+        event.preventDefault();
+        videoSearch();
+    })
+
+
+    function videoSearch() {
+        $("#videos").empty();
+        let search = $('#search').val();
+        $.get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyA-A85JoLyq11NziNGjOlWcwOJ-1wxSf8o&type=video&part=snippet&maxResults=4&q="+search,function(data){
+            console.log(data)
+            console.log(search)
+
+            data.items.forEach(item => {
+                let video = `
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                `
+                $("#videos").append(video)
+            })        
+        })
+}
+
+
+
+})
